c9cfe10ff9f5b5877e8dc99ca9e721bb347defb8
권신학 <ytype33@gmail.com>
2020-08-21 14:27:20 +0900

    BUG: fix background color bug

--- thumbnail.py
+++ thumbnail.py
@@ -22,6 +22,6 @@ for i in range(1,60):
     file_list = os.listdir(path)
     firstImage = [file for file in file_list if file.startswith("1.")]
     print(f"{path}/{firstImage[0]}")
-    originImage = cv2.imread(f"{path}/{firstImage[0]}")
+    originImage = cv2.imread(f"{path}/{firstImage[0]}", cv2.IMREAD_UNCHANGED)
     resizedImage = image_resize(originImage, height = 600)
     cv2.imwrite(f"thumbnail{i}.png", resizedImage)
